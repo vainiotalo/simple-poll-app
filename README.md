7515c7eb13ac7029012267b03418334c5a7e9b10
Petteri Vainiotalo <pevain@utu.fi>
2020-04-20 17:31:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## simple-poll-app
---
+++ poll-front/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Poll from './Poll';
+
+ReactDOM.render(
+    <Poll />,
+    document.getElementById('root')
+)
